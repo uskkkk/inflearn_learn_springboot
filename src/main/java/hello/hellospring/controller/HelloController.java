d40a745712e63ba6e9867146a66e41718c859bc2
uskkkk <dntjd4562@gmail.com>
2023-02-16 23:38:28 +0900

    2023-02-16 학습

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,9 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +14,48 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data","spring!!");
         return "hello";
+    }
+
+
+    @GetMapping(value = "hello-mvc")
+    public String helloMvc(@RequestParam(name = "name",required = false) String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    @GetMapping(value = "hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam(name = "name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping(value = "hello-api")
+    @ResponseBody Hello helloApi(@RequestParam(name = "name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+
+//           [@ResponseBody 사용]
+//        HTTP의 BODY에 문자 내용을 직접 반환
+//        'vieResolver' 대신 'HttpMessageConverter'가 동작
+//        기본 문자처리 : 'StringHttpMessageConverter'
+//        기본 객체처리 : 'MappingJacson2HttpMessageConverter'
+//        byte  처리 등등 기타 여러 HttpMessageConverter가 기본으로 등록
+    }
+    public
+
+    static class Hello {
+        private String name;
+
+        // property 접근 방식
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
     }
 }
---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,33 @@
+package hello.hellospring.domain;
+
+public class Member {
+
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Member{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
